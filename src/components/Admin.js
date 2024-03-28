992592de27af8c755495432d6e61c207fa4465d2
Lautaro <kautarol@gmail.com>
2024-03-28 15:43:30 -0300

    Starting frontend Admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import '../styles/Admin.css';
 
 export default function Administracion (){
+
+    const [adminEnter, setAdminEnter] = useState(false);
+    const password = "fotometro";
+
     return(
-        <div>
-            <h1>Esta es la página para el menú de administración de publicaciones.</h1>
-            <Link to="/">
-                <h2>Home</h2>
-            </Link>
-            <Link to="/">
-                <h2>Home</h2>
-            </Link>
-            <Link to="/">
-                <h2>Home</h2>
-            </Link>
-            <Link to="/">
-                <h2>Home</h2>
-            </Link>
+        <div className="admin-content">
+            <h1 className="admin-title">Administración.</h1>
+            {
+                adminEnter === false ?
+                <div className="pass-content">
+                    <p className="pass-label">Las siguientes páginas del sitio son exclusivas para el staff.</p>
+                    <p className="pass-label">Coloque la contraseña:</p>
+                </div>
+                : 
+                <div>
+                    <Link to="/">
+                        <h2>Home</h2>
+                    </Link>
+                    <Link to="/">
+                        <h2>Home</h2>
+                    </Link>
+                    <Link to="/">
+                        <h2>Home</h2>
+                    </Link>
+                    <Link to="/">
+                        <h2>Home</h2>
+                    </Link>
+                </div>
+            }  
         </div>
     )
 }
